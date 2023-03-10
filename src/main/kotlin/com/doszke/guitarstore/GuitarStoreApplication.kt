15e964622ecc06879ac1fb70a084d22ec9b67a88
doszke <jakub.siembida@interia.pl>
2023-03-10 20:16:36 +0100

    Initial commit

---
+++ src/main/kotlin/com/doszke/guitarstore/GuitarStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.doszke.guitarstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuitarStoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<GuitarStoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/doszke/guitarstore/GuitarStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.doszke.guitarstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GuitarStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
